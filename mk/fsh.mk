b43d5b3cc0925d8963ae749dbe4e4c0e472e924f
Dmitry Ponyatov <dponyatov@gmail.com>
2025-04-23 14:10:53 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- mk/fsh.mk
+++ mk/fsh.mk
@@ -5,9 +5,13 @@ APT    += dotnet-runtime-$(DOTNET_VER) dotnet-sdk-$(DOTNET_VER)
 dotnet: \
 	/etc/apt/trusted.gpg.d/microsoft.asc \
 	/etc/apt/sources.list.d/microsoft.list
-	$(MAKE) update
+	$(MAKE) update .config/dotnet-tools.json
 	dotnet tool install --global fantomas
 /etc/apt/trusted.gpg.d/microsoft.asc:
 	sudo $(CURL) $@ $(MS_URL)/keys/microsoft.asc
 /etc/apt/sources.list.d/microsoft.list:
 	sudo $(CURL) $@ $(MS_URL)/config/debian/12/prod.list
+.config/dotnet-tools.json:
+	dotnet new tool-manifest
+
+# dotnet tool update --ignore-failed-sources --global Microsoft.dotnet-interactive
