66f7042a1df926202b58daf857349680bd1e125b
Dmitry Ponyatov <dponyatov@gmail.com>
2025-04-24 20:45:53 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- mk/fsh.mk
+++ mk/fsh.mk
@@ -6,8 +6,8 @@ dotnet: \
 	/etc/apt/trusted.gpg.d/microsoft.asc \
 	/etc/apt/sources.list.d/microsoft.list
 	$(MAKE) update .config/dotnet-tools.json
-	dotnet tool install --global fantomas
-	dotnet tool install --global Microsoft.dotnet-interactive
+	dotnet tool install -v d -g fantomas
+	dotnet tool install -v d -g Microsoft.dotnet-interactive
 /etc/apt/trusted.gpg.d/microsoft.asc:
 	sudo $(CURL) $@ $(MS_URL)/keys/microsoft.asc
 /etc/apt/sources.list.d/microsoft.list:
