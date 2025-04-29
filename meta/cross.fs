f5c198abc7c1944ca99e48c4c095e885f6bd0608
Dmitry Ponyatov <dponyatov@gmail.com>
2025-04-29 12:35:31 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- meta/cross.fs
+++ meta/cross.fs
@@ -1,10 +1,37 @@
 module cross
 
-let _hw_cortex = [ "pillF030"; "pillF103"; "f429disco" ]
-let _hw = _hw_cortex @ [ "esp8266"; "pc" ]
+let _hw_cortex =
+    [ //
+      "pillF030"
+      "pillF103"
+      "f429disco" ]
 
-let _cpu = [ "i5"; "stm32f429zit"; "stm32f030f4p"; "stm32f103c8t"; "lx106" ]
+let _hw =
+    _hw_cortex
+    @ [ //
+        "esp8266"
+        "pc" ]
 
-let _arch = [ "x86_64"; "cortexM"; "cortexM0"; "cortexM3"; "cortexM4"; "xtensa" ]
+let _cpu =
+    [ //
+      "i5"
+      "stm32f429zit"
+      "stm32f030f4p"
+      "stm32f103c8t"
+      "lx106" ]
 
-let _os = [ "bare"; "linux"; "win32"; "rtos" ]
+let _arch =
+    [ //
+      "x86_64"
+      "cortexM"
+      "cortexM0"
+      "cortexM3"
+      "cortexM4"
+      "xtensa" ]
+
+let _os =
+    [ //
+      "bare"
+      "linux"
+      "win32"
+      "rtos" ]
--- meta/expr.fs
+++ meta/expr.fs
@@ -22,13 +22,13 @@ let rec lookup (env: env) (name: string) =
 // lookup glob "x"
 // lookup glob "c"
 
-let rec eval (e: expr) : int =
+let rec eval (e: expr) (env: env) : int =
     match e with
     | CstI i -> i
-    | Var v -> lookup glob v
-    | Prim("+", e1, e2) -> eval e1 + eval e2
-    | Prim("*", e1, e2) -> eval e1 * eval e2
-    | Prim("-", e1, e2) -> eval e1 - eval e2
+    | Var v -> lookup env v
+    | Prim("+", e1, e2) -> eval e1 env + eval e2 env
+    | Prim("*", e1, e2) -> eval e1 env * eval e2 env
+    | Prim("-", e1, e2) -> eval e1 env - eval e2 env
     | Prim _ -> failwith "unknown primitive"
 
 // eval c17 // 17
@@ -42,5 +42,5 @@ let baf = Var "baf"
 
 let e3a = Prim("+", CstI 3, Var "a") // 3+a
 let b9a = Prim("+", Prim("*", Var "b", CstI 9), Var "a") // b*9+a
-// eval e3a // 6
-// eval b9a // 1002
+// eval e3a glob // 6
+// eval b9a glob // 1002
