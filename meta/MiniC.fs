951d3c077ee4f8fc9133b562311d14465e395af9
Dmitry Ponyatov <dponyatov@gmail.com>
2025-04-30 14:26:14 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

---
+++ meta/MiniC.fs
@@ -0,0 +1,28 @@
+// http://timjones.io/blog/archive/2014/04/13/writing-a-minic-to-msil-compiler-in-fsharp-part-0-introduction
+
+// http://timjones.io/blog/archive/2014/04/20/writing-a-minic-to-msil-compiler-in-fsharp-part-1-defining-the-abstract-syntax-tree
+
+type Program = //
+    Decl list
+
+and Decl =
+    | VarDecl of VarDecl
+    | FunDecl of FunDecl
+
+and TypeSpec =
+    | Void
+    | Bool
+    | Int
+    | Float
+
+and VarDecl =
+    | ScalarDecl of TypeSpec * Id
+    | ArrayDecl of TypeSpec * Id
+
+and FunDecl = //
+    TypeSpec * Id * Params * CompoundStatement
+
+and Id = string
+
+and Params = //
+    VarDecl list
