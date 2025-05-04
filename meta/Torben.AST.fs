93bdfd8ea50f58467910919d7d9509e8fe28edcf
Dmitry Ponyatov <dponyatov@gmail.com>
2025-05-04 15:25:38 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- meta/Torben.AST.fs
+++ meta/Torben.AST.fs
@@ -6,16 +6,18 @@ module Torben.AST
 type AST = //
     Expr list
 
+and Literal = //
+    | Int of int
+    | Float of float
+    | Bool of bool
+
 and Expr = //
     | Literal of Literal
+    | Infix of Expr * Op * Expr
+    | Pfx of Op * Expr
 
 and Op = //
     | Add
     | Sub
     | Mul
     | Div
-
-and Literal = //
-    | Int of int
-    | Float of float
-    | Bool of bool
