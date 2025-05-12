5bbb81731be1c2aa466c94ab9696ec1182da1e19
Dmitry Ponyatov <dponyatov@gmail.com>
2025-05-12 14:42:10 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- meta/Siek.py
+++ meta/Siek.py
@@ -4,13 +4,27 @@
 # https://github.com/IUCompilerCourse/Essentials-of-Compilation/releases/tag/python-MIT-press
 
 class AST:
-    def __init__(self, value): self.value = value
+    def __init__(self, value): self.value = value; self.nest = []
     def tag(self): return self.__class__.__name__.lower()
     def val(self): return f'{self.value}'
-    def __repr__(self): return f'{self.tag()}:{self.val()}'
+    def head(self,prefix=''):
+        return f'{prefix}{self.tag()}:{self.val()}'
+    def dump(self,depth=0):
+        def tab(depth): return '\t'*depth
+        ret = self.head(prefix=tab(depth))
+        return ret
+    def __repr__(self): return self.dump()
 
 class Constant(AST): pass
 
 eight = Constant(8)
 eight
 
+class UnaryOp(AST):
+    def __init__(self, op, operand):
+        self.op = op
+        self.operand = operand
+
+## unary subtraction
+class USub(AST):
+    def __init__(self): super().__init__('-')
