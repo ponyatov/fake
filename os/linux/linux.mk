463e53164679fc214ac9d12c70e684926f873b83
Dmitry Ponyatov <dponyatov@gmail.com>
2025-05-13 08:29:39 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- os/linux/linux.mk
+++ os/linux/linux.mk
@@ -9,12 +9,15 @@ LINUX_CFG += $(CWD)/$(MODULE).linux
 LINUX_MAKE  = $(MAKE) -f /usr/src/linux-source-$(LINUX_VER)/Makefile
 LINUX_MAKE += ARCH=$(ARCH) CROSS_COMPILE=$(TARGET)-
 LINUX_MAKE += INSTALL_PATH=$(CWD)/root/boot
+LINUX_MAKE += INSTALL_MOD_PATH=$(CWD)/root
+LINUX_MAKE += INSTALL_HDR_PATH=$(CWD)/root/usr/include
+LINUX_MAKE += INSTALL_DTBS_PATH=$(CWD)/root/boot/dtbs
 
 .PHONY: linux
 linux: tmp/kernel/.config
 	cd tmp/kernel ;\
 	$(LINUX_MAKE) menuconfig && $(LINUX_MAKE) &&\
-	$(LINUX_MAKE) install
+	$(LINUX_MAKE) install modules_install headers_install dtbs_install
 
 tmp/kernel/.config: $(LINUX_CFG) mk/cross.mk os/linux/linux.mk
 	cd tmp/kernel ; rm .config ; \
